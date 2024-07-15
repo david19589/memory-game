85edd29a02756e5e97ec6ae5a78287cc8658efc6
david19589 <davitnasyidashvili2006@gmail.com>
2024-07-15 20:25:28 +0400

    added some styles in 'multiplayer' component and one span that shows current turn

--- src/components/multiplayer/index.tsx
+++ src/components/multiplayer/index.tsx
@@ -50,18 +50,18 @@ function MultiPlayer() {
           {players.slice(0, numberOfPlayers).map((player) => (
             <div
               key={player.id}
-              className="md:max-w-[10rem] relative max-w-[4rem] w-full"
+              className="lg:max-w-[16rem] md:max-w-[10rem] relative max-w-[4rem] w-full"
             >
               <span
                 className={clsx(
                   turn === player.id ? "flex" : "hidden",
-                  "md:translate-x-[4.5rem] w-[1rem] h-[1rem] bg-[#FDA214] rotate-45 absolute translate-x-[1.5rem] translate-y-[-0.4rem]"
+                  "lg:translate-x-[7.5rem] md:translate-x-[4.5rem] w-[1rem] h-[1rem] bg-[#FDA214] rotate-45 absolute translate-x-[1.5rem] translate-y-[-0.4rem]"
                 )}
               ></span>
               <div
                 className={clsx(
                   turn === player.id ? "bg-[#FDA214]" : "bg-[#DFE7EC]",
-                  "md:max-w-[10rem] md:justify-between md:items-start md:px-[1.5rem] md:py-[1.1rem] relative max-w-[4rem] flex flex-col items-center w-full p-[0.65rem] rounded-lg"
+                  "lg:flex-row lg:items-center lg:max-w-[16rem] md:max-w-[10rem] md:justify-between md:items-start md:px-[1.5rem] md:py-[1.1rem] relative max-w-[4rem] flex flex-col items-center w-full p-[0.65rem] rounded-lg"
                 )}
               >
                 <h3
@@ -75,7 +75,7 @@ function MultiPlayer() {
                 <h3
                   className={clsx(
                     turn === player.id ? "text-[#FCFCFC]" : "text-[#7191A5]",
-                    "md:flex hidden text-[1.15rem] leading-[1.2rem] font-[700] mb-[0.5rem]"
+                    "lg:mb-0 md:flex hidden text-[1.15rem] leading-[1.2rem] font-[700] mb-[0.5rem]"
                   )}
                 >
                   Player {player.name}
@@ -89,6 +89,14 @@ function MultiPlayer() {
                   {score[player.id - 1]}
                 </span>
               </div>
+              <span
+                className={clsx(
+                  turn === player.id ? "lg:flex" : "hidden",
+                  "hidden text-[0.9rem] leading-[1rem] tracking-[0.3rem] font-[700] text-[#152938] mt-[1rem] translate-x-[3rem]"
+                )}
+              >
+                CURRENT TURN
+              </span>
             </div>
           ))}
         </div>
